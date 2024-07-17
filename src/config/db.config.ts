5cfa8c6915ea57bf61f9f211f664fa77db385f24
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-07-17 14:53:20 +0100

    refactor: update code to return instance of db

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,16 +1,19 @@
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
+let db: any;
 
-async function connectToDb() {
+export default async function connectToDb() {
+  if (db) return db; // Return the existing database instance if it exists
   try {
     await client.connect();
     console.log("Connected to MongoDB");
+    db = client.db();
+    return db;
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
   }
 }
-
-module.exports = connectToDb;
